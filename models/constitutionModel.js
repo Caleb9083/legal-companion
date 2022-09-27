99d3cf4840d9ba0acc33f70068fad9e784f6e84c
Caleb9083 <=calebosam12@gmail.com>
2022-09-27 21:12:19 +0000

    Constition Chapter Model added

--- models/constitutionModel.js
+++ models/constitutionModel.js
@@ -1,18 +1,54 @@
-const { Schema, SchemaTypes, model } = require("mongoose")
+const { Schema, SchemaTypes, model } = require("mongoose");
 
-const ConstitutionSchema = new Schema({
-    title: {
-        type: SchemaTypes.String,
-        required: true
+// Constition Schema
+const ConstitutionSchema = new Schema(
+    {
+        title: {
+            type: SchemaTypes.String,
+            required: true,
+        },
+        preamble: {
+            type: SchemaTypes.String,
+            required: true,
+        },
     },
-    preamble: {
-        type: SchemaTypes.String,
-        required: true
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true
-});
+);
 
 const ConstitutionModel = model("Constitution", ConstitutionSchema);
 
-module.exports = ConstitutionModel
+// Constition Chapter Schema
+const ConstitutionChapterSchema = new Schema(
+    {
+        constitution: {
+            type: SchemaTypes.ObjectId,
+            ref: "Constitution",
+            required: true,
+        },
+        title: {
+            type: SchemaTypes.String, // Article I
+            required: true,
+        },
+        description: {
+            type: SchemaTypes.String, // The legislative branch
+            required: true,
+        },
+        isAmmendment: {
+            type: SchemaTypes.Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const ConstitutionChapterModel = model(
+    "ConstitutionChapter",
+    ConstitutionChapterSchema
+);
+
+module.exports = { ConstitutionModel, ConstitutionChapterModel };

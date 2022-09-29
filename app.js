5e0eca5fd0badbd625f10a94e7f97cb6c623b3f1
Caleb9083 <=calebosam12@gmail.com>
2022-09-29 23:06:30 +0000

    Chapter routers created and added to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const instrumentRouter = require("./routers/instrumentRouter");
 const constitutionRouter = require("./routers/constitutionRouter")
+const chapterRouter = require("./routers/chapterRouter")
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 //Routes
 app.use("/api/v1/instruments", instrumentRouter);
-app.use("/api/v2", constitutionRouter)
+app.use("/api/v2/constitutions", constitutionRouter)
+app.use("/api/v2/constitutions/:constitutionId/chapters", chapterRouter)
 
 module.exports = app;
---
+++ routers/chapterRouter.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const {
+    ConstitutionModel,
+    ConstitutionChapterModel,
+    ConstitutionSectionModel,
+} = require("../models/constitutionModel");
+const router = express.Router();
+
+router
+    .route("/")
+    // get chapters under constitution
+    .get(async (req, res) => {
+        const { search, page, pageSize, isAmmendment } = req.query;
+        const { constitutionId } = req.params;
+
+        if (search) {
+            const chapters = await ConstitutionChapterModel.find({
+                constitution: constitutionId,
+                $or: [
+                    { title: { $regex: search, $options: "im" } },
+                    { preamble: { $regex: search, $options: "im" } },
+                ],
+                ...(_.isBoolean(isAmmendment) ? { isAmmendment } : {}),
+            })
+                .skip(page * pageSize || 0)
+                .limit(pageSize || 0)
+                .lean();
+            return res.json(chapters).status(200);
+        }
+        const chapters = await ConstitutionChapterModel.find({
+            constitution: constitutionId,
+            ...(_.isBoolean(isAmmendment) ? { isAmmendment } : {}),
+        })
+            .skip(page * pageSize || 0)
+            .limit(pageSize || 0)
+            .lean();
+        return res.json(chapters).status(200);
+    })
+    // create chapter
+    .post(async (req, res) => {
+        const { constitutionId } = req.params;
+        const newChapter = new ConstitutionChapterModel({
+            constitution: constitutionId,
+            ...req.body,
+        });
+
+        await newChapter.save();
+        return res.json(newChapter.toJSON()).status(200);
+    });
+
+router
+    .route("/:chapterId")
+    // get single chapter under constitution
+    .get(async (req, res) => {
+        const { chapterId, constitutionId } = req.params;
+        const chapter = await ConstitutionChapterModel.findOne({
+            _id: chapterId,
+            constitution: constitutionId,
+        });
+        if (!chapter) {
+            return res.status(404).statusMessage("Chapter Not Found");
+        }
+        return res.json(chapter.toJSON()).status(200);
+    })
+    // update chapter
+    .put(async (req, res) => {
+        const { chapterId, constitutionId } = req.params;
+        const chapter = await ConstitutionChapterModel.findOne({
+            _id: chapterId,
+            constitution: constitutionId,
+        });
+        if (!chapter) {
+            return res.status(404).statusMessage("Chapter Not Found");
+        }
+        await chapter.updateOne({
+            $set: {
+                ...req.body,
+            },
+        });
+        return res.json(chapter.toJSON()).status(200);
+    })
+    // delete chapter
+    .delete(async (req, res) => {
+        const { chapterId, constitutionId } = req.params;
+        const chapter = await ConstitutionChapterModel.findOne({
+            _id: chapterId,
+            constitution: constitutionId,
+        });
+        if (!chapter) {
+            return res.status(404).statusMessage("Chapter Not Found");
+        }
+        await chapter.deleteOne();
+        return res.json(chapter.toJSON()).status(200);
+    });
+
+module.exports = router;

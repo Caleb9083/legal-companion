89f5e8f01fde0b84fbb9d78a06d90961ec806a8f
Caleb9083 <=calebosam12@gmail.com>
2022-09-30 14:49:27 +0000

    Only Constitutions routes active

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 //Routes
-app.use("/api/v1/instruments", instrumentRouter);
+//app.use("/api/v1/instruments", instrumentRouter);
 app.use("/api/v2/constitutions", constitutionRouter, chapterRouter, sectionRouter)
-
 
 module.exports = app;

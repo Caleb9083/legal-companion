b68b0e317c825d0a8c7b2e94b96eea99242791f4
Caleb9083 <=calebosam12@gmail.com>
2022-10-01 11:24:08 +0000

    Cors disabled

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const instrumentRouter = require("./routers/instrumentRouter");
 const constitutionRouter = require("./routers/constitutionRouter")
 const chapterRouter = require("./routers/chapterRouter")
 const sectionRouter = require("./routers/sectionRouter")
-
+const cors = require('cors')
 const app = express();
 
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors())
 
 //Routes
 //app.use("/api/v1/instruments", instrumentRouter);

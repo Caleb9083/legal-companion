98ddb37733de1c8106b5384b9e1f6e919020a19a
Caleb9083 <calebosam12@gmail.com>
2022-12-14 11:02:16 +0000

    Remove uneccessary code in chapter router

--- routers/chapterRouter.js
+++ routers/chapterRouter.js
@@ -1,22 +1,16 @@
 const express = require("express");
-const ConstitutionChapterModel = require("../models/constitutionModel");
 const chapterController = require("../controllers/chapterController")
 const router = express.Router();
 
 router
     .route("/:constitutionId/chapters")
-    // get chapters under constitution
     .get(chapterController.getAllChapterForConstitution)
-    // create chapter
     .post(chapterController.createChapter);
 
 router
     .route("/:constitutionId/chapters/:chapterId")
-    // get single chapter under constitution
     .get(chapterController.getOneChapterForConstitution)
-    // update chapter
     .put(chapterController.updateChapterForConstitution)
-    // delete chapter
     .delete(chapterController.deleteChapterForConstitution);
 
 module.exports = router;

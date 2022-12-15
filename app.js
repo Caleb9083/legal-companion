0b5e0f01c6c8040faaf8c4f469fe092903c6f837
Caleb9083 <calebosam12@gmail.com>
2022-12-15 08:53:40 +0000

    Create global error handler to handle errors in appp.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const chapterRouter = require("./routers/chapterRouter");
 const sectionRouter = require("./routers/sectionRouter");
 const userRouter = require("./routers/userRouter");
 const cors = require("cors");
+const AppError = require("./utils/appError");
+
 const app = express();
 
 //Middlewares
@@ -15,11 +17,24 @@ app.use(cors());
 
 //Routes
 app.use(
-    "/api/v2/constitutions",
-    constitutionRouter,
-    chapterRouter,
-    sectionRouter
+  "/api/v2/constitutions",
+  constitutionRouter,
+  chapterRouter,
+  sectionRouter
 );
 app.use("/api/v2/auth", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
+});
+
+app.use((err, req, res, next) => {
+  res.status(200).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+});
 
 module.exports = app;
